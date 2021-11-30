ddc1acca7f7f19b4b337abe422f7b37f082043bb
meymeynard <meymeynard@gmail.com>
2021-12-01 06:45:30 +0800

    chore: disable source maps for dev-tools

--- build-utils/webpack.production.js
+++ build-utils/webpack.production.js
@@ -1,8 +1,11 @@
 // build_utils/webpack.production.js
 
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin")
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin")
 
 module.exports = () => ({
+  devtool: "nosources-source-map",
   output: {
     filename: "production.js",
   },
@@ -16,6 +19,16 @@ module.exports = () => ({
           "sass-loader"
         ]
       }
+    ]
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true
+      }),
+      new OptimizeCSSAssetsPlugin({})
     ]
   },
   plugins: [ new MiniCssExtractPlugin() ]
