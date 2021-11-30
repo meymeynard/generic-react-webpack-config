7ffaec12d9c51e066839accca035398653e627fa
meymeynard <meymeynard@gmail.com>
2021-12-01 04:25:23 +0800

    chore: checks if there is a change in the app and fires render method if true

--- src/index.js
+++ src/index.js
@@ -14,3 +14,15 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+if (module.hot && process.env.NODE_ENV === "development") {
+  module.hot.accept("./App", () => {
+    const NextApp = require("./App").default
+    ReactDOM.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+      rootId
+    )
+  })
+}
