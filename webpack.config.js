558d2ce1c15e07a6c08dabcfe77e669c2e469195
meymeynard <meymeynard@gmail.com>
2021-12-01 03:39:04 +0800

    chore: add and configure loaders

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,20 @@ module.exports = ({ mode } = { mode: "production" }) => {
       path: path.resolve(__dirname__, "build"),
       filename: "bundled.js"
     },
+    module: {
+      rules: [
+        {
+          test: /\.jpe?g|png$/,
+          exclude: /node_modules/,
+          use: ["url-loader", "file-loader"]
+        },
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          loader: "babel-loader"
+        }
+      ]
+    },
     plugins: [
       new HtmlWebpackPlugin({
         template: "./public/index.html"
